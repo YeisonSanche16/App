79f3b115cb24d41f92293b56b466595b6b7c6920
William Gomez <william.gomex@gmail.com>
2019-02-23 13:57:56 -0500

    feat/configuration: Configuration with gulp

---
+++ app/Js/components/button/button.js
@@ -0,0 +1,9 @@
+const hello = (name) => {
+  return `hello ${name}`;
+};
+const odds = evens.map((v) => v + 1);
+const nums = evens.map((v, i) => v + i);
+setTimeout(() => {
+  console.log('ES2015 FTW');
+}, 1000);
+console.log('Hola');
